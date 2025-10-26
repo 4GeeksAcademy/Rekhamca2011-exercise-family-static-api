c58c9da1c75b74d3c9cff35b04c1113c749601da
Rekhamca2011 <rekhamca2011@gmail.com>
2025-10-26 02:07:03 +0000

    Family Static API with Flask

--- src/datastructures.py
+++ src/datastructures.py
@@ -5,6 +5,7 @@ Update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
@@ -13,9 +14,23 @@ class FamilyStructure:
             {
                 "id": self._generate_id(),
                 "first_name": "John",
-                "last_name": last_name,
+                "last_name": "Jackson",
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": "Jackson",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": "Jackson",
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
@@ -26,19 +41,39 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+
+        # member = jackson_family.add_member(new_member)
+        # return jsonify(member), 200
+
+        if member.get("id", None) is None:
+            member["id"] = self._generate_id()
+        member["last_name"] = self.last_name
+
+        # You have to implement this method
+        # Append the member to the list of _members
+
+        self._members.append(member)
+        return member
+        
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        # You have to implement this method
+        # Loop the list and delete the member with the given id
+        for index, member in enumerate(self._members):
+            if member["id"] == id:
+                removed_member = self._members.pop(index)
+                return removed_member
+        return None
+        
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        # You have to implement this method
+        # Loop all the members and return the one with the given id
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
+        
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
